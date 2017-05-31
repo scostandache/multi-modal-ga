688eec18ecbdf7dd5f7894ff5d869ef8c51c7759
Serban Costandache <serbanc94@gmail.com>
2017-05-31 22:41:53 +0300

    added test results

--- GA.py
+++ GA.py
@@ -1,3 +1,4 @@
+#todo-me create separate function for visualising
 from population import Population
 import numpy as np
 from matplotlib import pyplot as plt
@@ -20,58 +21,56 @@ class GA(object):
                                 precision,
                                 fitness)
 
-        # for i in xrange(gen_no):
-        #     niche_method(population, fitness,**kwargs)
-        #     if (i % 10 == 0):
-        #
-        #         population.sort()
-        #         print population.MEMBERS[0].fitness
+        for i in xrange(gen_no):
+            niche_method(population, fitness,**kwargs)
+        population.sort()
+        return population.MEMBERS[0].fitness
 
 
 
         # ===============================
 
 
-
-        x = np.array([])
-        y = np.array([])
-        z = np.array([])
-        count = 0
-        def update_graph(count):
-
-            x = np.array([member.params_float[0] for member in population.MEMBERS])
-            y = np.array([member.params_float[1] for member in population.MEMBERS])
-            z = np.array([ member.fitness for member in population.MEMBERS ])
-
-            graph.set_data(x, y)
-            graph.set_3d_properties(z)
-
-            ax.set_xlim3d(tuple(LIMITS[0].values()))
-            ax.set_ylim3d(tuple(LIMITS[0].values()))
-            ax.set_zlim3d(min([member.fitness for member in population.MEMBERS]) - 3,
-                          max([member.fitness for member in population.MEMBERS]) + 3)
-
-
-            niche_method(population, fitness,**kwargs)
-            population.sort()
-            best = population.MEMBERS[0].fitness
-            title.set_text('f = {}, n = {}, t={}, best={}'.format(fitness.__name__[:4], niche_method.__name__[:3],count,best))
-            return title, graph,
-
-
-        fig = plt.figure()
-
-        ax = fig.add_subplot(111, projection = '3d')
-
-        ax.set_xlabel('first argument')
-        ax.set_ylabel('second argument')
-        ax.set_zlabel('fitness')
-
-        title = ax.set_title('')
-        graph, = ax.plot(xs=x, ys=y, zs=z, linestyle="", marker=".", color='k')
-        ani = matplotlib.animation.FuncAnimation(fig, update_graph, gen_no, interval=1, blit = True, repeat=True,  repeat_delay= 5000)
-
-        plt.show()
+        #
+        # x = np.array([])
+        # y = np.array([])
+        # z = np.array([])
+        # count = 0
+        # def update_graph(count):
+        #
+        #     x = np.array([member.params_float[0] for member in population.MEMBERS])
+        #     y = np.array([member.params_float[1] for member in population.MEMBERS])
+        #     z = np.array([ member.fitness for member in population.MEMBERS ])
+        #
+        #     graph.set_data(x, y)
+        #     graph.set_3d_properties(z)
+        #
+        #     ax.set_xlim3d(tuple(LIMITS[0].values()))
+        #     ax.set_ylim3d(tuple(LIMITS[0].values()))
+        #     ax.set_zlim3d(min([member.fitness for member in population.MEMBERS]) - 3,
+        #                   max([member.fitness for member in population.MEMBERS]) + 3)
+        #
+        #
+        #     niche_method(population, fitness,**kwargs)
+        #     population.sort()
+        #     best = population.MEMBERS[0].fitness
+        #     title.set_text('f = {}, n = {}, t={}, best={}'.format(fitness.__name__[:4], niche_method.__name__[:3],count,best))
+        #     return title, graph,
+        #
+        #
+        # fig = plt.figure()
+        #
+        # ax = fig.add_subplot(111, projection = '3d')
+        #
+        # ax.set_xlabel('first argument')
+        # ax.set_ylabel('second argument')
+        # ax.set_zlabel('fitness')
+        #
+        # title = ax.set_title('')
+        # graph, = ax.plot(xs=x, ys=y, zs=z, linestyle="", marker=".", color='k')
+        # ani = matplotlib.animation.FuncAnimation(fig, update_graph, gen_no, interval=1, blit = True, repeat=True,  repeat_delay= 5000)
+        #
+        # plt.show()
 
 
 
--- main.py
+++ main.py
@@ -6,19 +6,34 @@ import niche
 if __name__ == '__main__':
 
     galg = GA()
-    galg.solve(gen_no=4000,
-               niche_method = niche.RTS,
-               mut_prob = .4,
-               cross_prob = .6,
-               window=20,
-               pop_size=50,
-               LIMITS=fitness.dispatcher['SHCB']['LIMITS'],
-               param_no=2,
-               precision=7,
-               fitness=fitness.dispatcher['SHCB']['function']
-               )
 
+    for niche_met in [(niche.deterministic_crowding,150),(niche.RTS,4000)]:
 
+        with open(niche_met[0].__name__+'/raport.txt', 'w+') as f:
+            print niche_met[0].__name__
+            for function in ['rastrigin','griewangk','rosenbrock']:
+
+                print function
+                for no_params in [5,10,30]:
+                    line = ''
+                    print no_params
+                    fitness_mean = 0.0
+                    for _ in xrange(3):
+                        result = galg.solve(gen_no=niche_met[1],
+                                            niche_method=niche_met[0],
+                                            mut_prob=.8,
+                                            cross_prob=.6,
+                                            window=20,
+                                            pop_size=80,
+                                            LIMITS=fitness.dispatcher[function]['LIMITS'],
+                                            param_no=no_params,
+                                            precision=5,
+                                            fitness=fitness.dispatcher[function]['function']
+                                            )
+                        fitness_mean+=result
+                    fitness_mean = fitness_mean/30.0
+                    line += function+' '+str(no_params)+' '+str(fitness_mean)+'\n'
+                    f.write(line)
 
 
 
